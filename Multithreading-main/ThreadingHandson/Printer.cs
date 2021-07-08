513e0640d48305390cc2dfd93a819a7dceda1acd
vishakhshetty <57668627+vishakhshetty@users.noreply.github.com>
2021-07-08 10:34:55 +0530

    Add files via upload

---
+++ Multithreading-main/ThreadingHandson/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingHandsOn
+{
+    class Printer
+    {
+        public void PrintNumber()
+        {
+            Console.WriteLine("->{0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+            //Print out numbers.
+
+            Console.WriteLine("Your numbers: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}, ", i);
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Multithreading-main/ThreadingHandson/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingHandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****ThreadStart Delegate App***");
+            Console.WriteLine("Do You Want [1] or [2] threads?");
+            string threadCount = Console.ReadLine();
+
+            //Name the current Thread
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            //Display Thread Info
+
+            Console.WriteLine("-> {0} is executing Main()", Thread.CurrentThread.Name);
+
+
+            //Make Worker Class
+
+            Printer p = new Printer();
+
+            switch (threadCount)
+            {
+                case "2":
+                    //now make the thread.
+                    //give only method name
+                    Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumber))
+                    {
+                        Name = "Secondary"
+                    };
+                    backgroundThread.Start();
+                    //changes the state of current instance to ThreadState.Running
+
+                    break;
+                case "1":
+                    p.PrintNumber();
+                    break;
+                default:
+                    Console.WriteLine("I don't know what you want... you get 1 Thread");
+                    goto case "1";
+
+            }
+
+            Thread.Sleep(1000);
+            //Do some additional work.
+            Console.WriteLine("Hello this from main!");
+
+        }
+    }
+}
